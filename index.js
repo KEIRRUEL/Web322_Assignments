dc9eaa7dca12a31c6d9243a7bf3db88fa7d8ab77
Keir Ruel Lazara <klazara@myseneca.ca>
2024-11-27 09:08:11 -0500

    vercel bug

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const app = express();
 // Set the HTTP port to an environment variable or default to 3838
 const HTTP_PORT = process.env.PORT || 3838;
 
-app.set("views",__dirname + '/views');
+app.set("views",__dirname + "/views");
 app.set('view engine', 'ejs');
 // Serve static files from the "public" directory (e.g., CSS, JS files, images)
 app.use(express.static("public"));
